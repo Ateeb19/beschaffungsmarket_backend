872cac7e03ebf85e9e64f5e5ac41895ace4e5a39
Ateeb Haque <135836945+Ateeb19@users.noreply.github.com>
2025-10-06 22:58:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const paymentRoutes = require("./routes/paymentRoutes");
 const app = express();
 app.use(
   cors({
-    origin: ["https://beschaffungsmarkt.com","http://localhost:3000"],
+    origin: ["https://beschaffungsmarkt.com","http://localhost:3000","https://beschaffungsmarkt01.onrender.com"],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
   })
@@ -45,3 +45,4 @@ app.use("/api", routes);
 const port = process.env.PORT || 5001;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
+
